19ea1fb549bcb8af846e1f3b8dc2bb782dd87d4a
genilson-alves <genilson.alves9555@gmail.com>
2023-06-30 10:56:59 -0300

    Fifth Commit.

---
+++ script.js
@@ -0,0 +1,13 @@
+var header = document.getElementById("");
+var navigation = document.getElementById("navigation");
+var content = document.getElementById("");
+var showSideBar = false;
+
+function toggleSidebar() {
+  showSideBar = !showSideBar;
+  if (showSideBar) {
+    navigation.style.marginLeft = "0";
+  } else {
+    navigation.style.marginLeft = "-100vw";
+  }
+}
