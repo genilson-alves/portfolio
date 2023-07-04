ac842f9f88344f01a894bbdec993d6a93ab3c6f6
genilson-alves <genilson.alves9555@gmail.com>
2023-07-04 12:08:30 -0300

    Fully responsibility added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var header = document.getElementById("");
 var navigation = document.getElementById("nav-links");
+var portfolioNavigation = document.getElementById("portfolio-navigation");
 var content = document.getElementById("");
 var showSideBar = false;
 
@@ -7,7 +8,9 @@ function toggleSidebar() {
   showSideBar = !showSideBar;
   if (showSideBar) {
     navigation.style.marginLeft = "0%";
+    portfolioNavigation.style.marginTop = "0px";
   } else {
     navigation.style.marginLeft = "-200%";
+    portfolioNavigation.style.marginTop = "20px";
   }
 }
