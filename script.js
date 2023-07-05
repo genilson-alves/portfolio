eba572279b6189b1ae8139ee30ea9600859b15ac
genilson-alves <genilson.alves9555@gmail.com>
2023-07-05 12:08:50 -0300

    Almost finished portfolio.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-var header = document.getElementById("");
 var navigation = document.getElementById("nav-links");
 var portfolioNavigation = document.getElementById("portfolio-navigation");
-var content = document.getElementById("");
+var content = document.getElementById("main-page");
 var showSideBar = false;
 
 function toggleSidebar() {
   showSideBar = !showSideBar;
-  if (showSideBar) {
-    navigation.style.marginLeft = "0%";
+  if (window.innerWidth < 797 && showSideBar) {
+    navigation.style.marginTop = "0%";
+    navigation.style.animationName = "showSidebar";
+    navigation.style.position = "fixed";
     portfolioNavigation.style.marginTop = "0px";
   } else {
-    navigation.style.marginLeft = "-200%";
+    navigation.style.marginTop = "-200%";
     portfolioNavigation.style.marginTop = "20px";
+    navigation.style.animationName = "";
   }
 }
+
+window.addEventListener("resize", (event) => {
+  if (window.innerWidth > 797 && showSideBar) {
+    toggleSidebar();
+  }
+});
