c750164d4fb95f4dcbb0dd6de51e98b58270e9e9
genilson-alves <genilson.alves9555@gmail.com>
2023-09-14 10:42:19 -0300

    Translation for english added.

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 var portfolioNavigation = document.getElementById("portfolio-navigation");
 var navigation = document.getElementById("nav-links");
 var content = document.getElementById("main-page");
+var portugueseBttn = document.querySelector("[data-brazil-flag]");
 var showSideBar = false;
+
+document.querySelector("[data-english-flag]").addEventListener("click", () => {
+  document.querySelector(".portuguese").style.display = "none";
+  document.querySelector(".english").style.display = "block";
+  document.querySelector("[data-nav-logo]").textContent = "Me.me";
+  document.querySelector("[data-nav-about]").textContent = "About";
+  document.querySelector("[data-nav-technologies]").textContent =
+    "Technologies";
+  document.querySelector("[data-nav-projects]").textContent = "Projects";
+  document.querySelector("[data-nav-contact]").textContent = "Contact";
+  document.querySelector("[data-english-flag]").style.display = "none";
+  document.querySelector("[data-brazil-flag]").style.display = "block";
+});
+
+if (portugueseBttn) {
+  document.querySelector("[data-brazil-flag]").addEventListener("click", () => {
+    document.querySelector(".portuguese").style.display = "block";
+    document.querySelector(".english").style.display = "none";
+    document.querySelector("[data-english-flag]").style.display = "block";
+    document.querySelector("[data-brazil-flag]").style.display = "none";
+  });
+}
 
 function toggleSidebar() {
   showSideBar = !showSideBar;
